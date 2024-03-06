3b7a57aad412ac1ee9bdb01a918c274c7257f89a
Rybalkoyuliia <rybalkoyuliia4@gmail.com>
2024-03-06 12:22:11 -0700

    instal dependencies, changed package json, deleted persistor

--- src/components/redux/store.js
+++ src/components/redux/store.js
@@ -1,37 +1,9 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { phonebookReducer } from './slice';
 
-import {
-  persistStore,
-  persistReducer,
-  FLUSH,
-  REHYDRATE,
-  PAUSE,
-  PERSIST,
-  PURGE,
-  REGISTER,
-} from 'redux-persist';
-import storage from 'redux-persist/lib/storage';
-
-const persistConfig = {
-  key: 'root',
-  version: 1,
-  storage,
-};
-
-const persistedReducer = persistReducer(persistConfig, phonebookReducer);
-
 export const store = configureStore({
   reducer: {
-    phonebook: persistedReducer,
+    phonebook: phonebookReducer,
   },
-  middleware: getDefaultMiddleware =>
-    getDefaultMiddleware({
-      serializableCheck: {
-        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
-      },
-    }),
   devTools: process.env.NODE_ENV !== 'production',
 });
-
-export let persistor = persistStore(store);
